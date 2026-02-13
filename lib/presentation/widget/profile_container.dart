1c5fb5afa44df82b985a64be2beb81631f77ea86
suehng0823 <s25010@gsm.hs.kr>
2026-02-13 23:49:58 +0900

    :recycle:: Light mode 추가, Container 크기 수정

--- lib/presentation/widget/profile_container.dart
+++ lib/presentation/widget/profile_container.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:project_setting/core/theme/app_colors.dart';
+import 'package:project_setting/core/theme/app_icons.dart';
 import 'package:project_setting/core/theme/app_text_styles.dart';
+import 'package:project_setting/presentation/widget/outing_status.dart';
 import 'package:project_setting/presentation/widget/time_display.dart';
 
-enum OutingStatus { waiting, approved, rejected, admin }
+bool isLight = true;
 
 class ProfileContainer extends StatefulWidget {
   final String name;
-  final String grade;
+  final int grade;
   final String major;
-  final String lateCount;
+  final int lateCount;
   final OutingStatus status;
   final bool onTime;
 
@@ -20,7 +22,7 @@ class ProfileContainer extends StatefulWidget {
     required this.grade,
     required this.major,
     required this.lateCount,
-    required this.status, // 외출상태
+    required this.status,
     required this.onTime,
   });
 
@@ -29,58 +31,31 @@ class ProfileContainer extends StatefulWidget {
 }
 
 class _ProfileContainerState extends State<ProfileContainer> {
-  String _getStatusText() {
-    switch (widget.status) {
-      case OutingStatus.waiting:
-        return '외출 대기 중';
-      case OutingStatus.approved:
-        return '외출중';
-      case OutingStatus.rejected:
-        return '외출 금지';
-      case OutingStatus.admin:
-        return '학생회';
-    }
-  }
 
-  Color _getStatusColor() {
-    switch (widget.status) {
-      case OutingStatus.waiting:
-        return AppColors.textSecondaryDark;
-      case OutingStatus.approved:
-        return AppColors.primary;
-      case OutingStatus.rejected:
-        return AppColors.negative;
-      case OutingStatus.admin:
-        return AppColors.admin;
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 84,
-      width: 312,
+      width: double.infinity,
       decoration: BoxDecoration(
-        color: AppColors.surfaceDark,
+        color: isLight ? AppColors.surface : AppColors.surfaceDark,
         borderRadius: BorderRadius.circular(8),
       ),
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Row(
           children: [
-            widget.onTime
-                ? Align(
-                    alignment: Alignment.centerLeft,
-                    child: CircleAvatar(
-                      radius: 26,
-                      child: SvgPicture.asset(
-                        'assets/icons/profile_circle.svg',
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                  )
-                : const SizedBox.shrink(),
-            widget.onTime ? const SizedBox(width: 12) : const SizedBox.shrink(),
+            if (widget.onTime) ...[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: CircleAvatar(
+                  radius: 26,
+                  child: AppIcons.profileCircle()
+                ),
+              ),
+            ],
+            if (widget.onTime) ...[const SizedBox(width: 12)],
             Expanded(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -93,14 +68,14 @@ class _ProfileContainerState extends State<ProfileContainer> {
                         Text(
                           widget.name,
                           style: AppTextStyles.title3.copyWith(
-                            color: AppColors.background,
+                            color: isLight ? AppColors.textPrimary : AppColors.background
                           ),
                         ),
                         const SizedBox(width: 8),
                         Text(
                           '${widget.grade}기 | ${widget.major}과',
                           style: AppTextStyles.caption1.copyWith(
-                            color: AppColors.textTertiaryDark,
+                            color: isLight ? AppColors.textButton : AppColors.textTertiaryDark
                           ),
                         ),
                       ],
@@ -109,7 +84,7 @@ class _ProfileContainerState extends State<ProfileContainer> {
                   Text(
                     '지각횟수: ${widget.lateCount}회',
                     style: AppTextStyles.text3.copyWith(
-                      color: AppColors.textSecondaryDark,
+                      color: isLight ? AppColors.textTertiary : AppColors.textSecondaryDark
                     ),
                   ),
                 ],
@@ -122,20 +97,19 @@ class _ProfileContainerState extends State<ProfileContainer> {
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   Text(
-                    _getStatusText(),
+                    widget.status.StatusText,
                     style: AppTextStyles.text1.copyWith(
-                      color: _getStatusColor(),
+                      color: widget.status.StatusColor,
                     ),
                   ),
-                  Flexible(
-                    child: widget.onTime
-                        ? const SizedBox.shrink()
-                        : TimeDisplay(
-                            onTime: widget.onTime,
-                            style: AppTextStyles.heavy,
-                            color: AppColors.textTertiaryDark,
-                          ),
-                  ),
+                  if (!widget.onTime) ...[
+                    Flexible(
+                      child: TimeDisplay(
+                        style: AppTextStyles.heavy,
+                        color: isLight ? AppColors.textButton : AppColors.textTertiaryDark,
+                      ),
+                    ),
+                  ],
                 ],
               ),
             ),

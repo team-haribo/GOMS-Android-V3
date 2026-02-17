cb174d9bd638bb46621692def6c2cff2a76a1134
aiden30015 <loolloo080313@gmail.com>
2026-02-17 18:25:32 +0900

    ✨:: main.dart 파일을 수정하여 MyApp 위젯을 추가하고 라우터 설정을 적용함

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_setting/core/router/app_router.dart';
 import 'package:project_setting/core/theme/app_theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MaterialApp(
-      title: 'App Name',
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'GOMS',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
       themeMode: ThemeMode.system,
-      home: const HomePage(),
-    ),
-  );
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home')),
-      body: const Center(child: Text('Hello, Flutter!')),
+      routerConfig: router,
     );
   }
 }

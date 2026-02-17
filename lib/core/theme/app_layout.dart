48edcf3aaf417970454942ace430f73e12d8bce6
suehng0823 <s25010@gsm.hs.kr>
2026-02-17 23:51:36 +0900

    :recycle:: SizedBox 레이아웃 파일로 분리

---
+++ lib/core/theme/app_layout.dart
@@ -0,0 +1,12 @@
+import 'dart:core';
+import 'package:flutter/material.dart';
+
+class AppSpacing {
+  static const double s4 = 4;
+}
+
+class AppGap {
+  static const SizedBox h4  = SizedBox(width: AppSpacing.s4);
+  static const SizedBox v4  = SizedBox(height: AppSpacing.s4);
+
+}

8adbfb8c0a18d98a8cad4ea6028b85927daf549b
suehng0823 <s25010@gsm.hs.kr>
2026-02-17 23:53:21 +0900

    :recycle:: SizedBox 레이아웃 파일로 분리

--- lib/presentation/map_page/widget/place_container.dart
+++ lib/presentation/map_page/widget/place_container.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:project_setting/core/theme/app_colors.dart';
 import 'package:project_setting/core/theme/app_icons.dart';
+import 'package:project_setting/core/theme/app_layout.dart';
 import 'package:project_setting/core/theme/app_text_styles.dart';
 
 class PlaceContainer extends StatefulWidget {
@@ -58,7 +59,7 @@ class _PlaceContainerState extends State<PlaceContainer> {
                             color: AppColors.mainTextDark,
                           ),
                         ),
-                        const SizedBox(width: 4),
+                        AppGap.h4,
                         Text(
                           widget.category,
                           style: AppTextStyles.caption2.copyWith(
@@ -67,7 +68,7 @@ class _PlaceContainerState extends State<PlaceContainer> {
                         ),
                       ],
                     ),
-                    const SizedBox(height: 4),
+                    AppGap.v4,
                     Text(
                       widget.address,
                       maxLines: 1,
@@ -76,7 +77,7 @@ class _PlaceContainerState extends State<PlaceContainer> {
                         color: AppColors.sub2Dark,
                       ),
                     ),
-                    const SizedBox(height: 4),
+                    AppGap.v4,
                     Row(
                       children: [
                         Text(
@@ -85,7 +86,7 @@ class _PlaceContainerState extends State<PlaceContainer> {
                             color: AppColors.sub2Dark,
                           ),
                         ),
-                        const SizedBox(width: 4),
+                        AppGap.h4,
                         Text(
                           widget.review >= 10
                               ? '${widget.review}+'
@@ -94,21 +95,21 @@ class _PlaceContainerState extends State<PlaceContainer> {
                             color: AppColors.sub2Dark,
                           ),
                         ),
-                        const SizedBox(width: 4),
+                        AppGap.h4,
                         Text(
                           '|',
                           style: AppTextStyles.caption2.copyWith(
                             color: AppColors.sub2Dark,
                           ),
                         ),
-                        const SizedBox(width: 4),
+                        AppGap.h4,
                         Text(
                           '추천',
                           style: AppTextStyles.caption2.copyWith(
                             color: AppColors.sub2Dark,
                           ),
                         ),
-                        const SizedBox(width: 4),
+                        AppGap.h4,
                         Text(
                           widget.recommended >= 10
                               ? '${widget.recommended}+'
@@ -128,12 +129,13 @@ class _PlaceContainerState extends State<PlaceContainer> {
               child: IconButton(
                 onPressed: () {
                   setState(() {
+                    // TODO: 상태관리 변경
                     isLiked = !isLiked;
                   });
                 },
                 icon: isLiked
-                    ? AppIcons.heartFilledIcon(width: 24, height: 24)
-                    : AppIcons.heartIcon(width: 24, height: 24),
+                    ? AppIcons.heartFilled(width: 24, height: 24)
+                    : AppIcons.heart(width: 24, height: 24),
               ),
             ),
           ],

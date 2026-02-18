8cf818ff1d40d5ccb9efaa635b1a7a8d2ed73a8b
suehng0823 <s25010@gsm.hs.kr>
2026-02-19 01:54:02 +0900

    :recycle:: 작성일이 현재 날짜로 표시되던 문제 수정

--- lib/presentation/map_page/widget/place_review_container.dart
+++ lib/presentation/map_page/widget/place_review_container.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:project_setting/core/theme/app_colors.dart';
 import 'package:project_setting/core/theme/app_icons.dart';
@@ -7,11 +5,12 @@ import 'package:project_setting/core/theme/app_layout.dart';
 import 'package:project_setting/core/theme/app_text_styles.dart';
 import 'package:intl/intl.dart';
 
-class PlaceReviewContainer extends StatefulWidget {
+class PlaceReviewContainer extends StatelessWidget {
   final String placeName;
   final String category;
   final String address;
   final String reviewDetailContent;
+  final DateTime createdAt;
 
   const PlaceReviewContainer({
     super.key,
@@ -19,32 +18,10 @@ class PlaceReviewContainer extends StatefulWidget {
     required this.category,
     required this.address,
     required this.reviewDetailContent,
+    required this.createdAt,
   });
 
-  @override
-  State<PlaceReviewContainer> createState() => _PlaceReviewContainerState();
-}
-
-class _PlaceReviewContainerState extends State<PlaceReviewContainer> {
   final bool isLiked = false;
-  late Timer _timer;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    _timer = Timer.periodic(const Duration(minutes: 1), (timer) {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    _timer.cancel();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -71,7 +48,7 @@ class _PlaceReviewContainerState extends State<PlaceReviewContainer> {
                       crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
-                          widget.placeName,
+                          placeName,
                           maxLines: 1,
                           overflow: TextOverflow.ellipsis,
                           style: AppTextStyles.text2.copyWith(
@@ -80,7 +57,7 @@ class _PlaceReviewContainerState extends State<PlaceReviewContainer> {
                         ),
                         const SizedBox(width: 4),
                         Text(
-                          widget.category,
+                          category,
                           style: AppTextStyles.caption2.copyWith(
                             color: AppColors.gray3,
                           ),
@@ -89,7 +66,7 @@ class _PlaceReviewContainerState extends State<PlaceReviewContainer> {
                     ),
                     const SizedBox(height: 4),
                     Text(
-                      widget.address,
+                      address,
                       maxLines: 1,
                       overflow: TextOverflow.ellipsis,
                       style: AppTextStyles.caption2.copyWith(
@@ -100,16 +77,16 @@ class _PlaceReviewContainerState extends State<PlaceReviewContainer> {
                     Row(
                       children: [
                         Text(
-                          widget.reviewDetailContent.length > 10
-                              ? '${widget.reviewDetailContent.substring(0, 10)}···'
-                              : widget.reviewDetailContent,
+                          reviewDetailContent.length > 10
+                              ? '${reviewDetailContent.substring(0, 10)}···'
+                              : reviewDetailContent,
                           style: AppTextStyles.caption1.copyWith(
                             color: AppColors.sub2Dark,
                           ),
                         ),
                         AppGap.h4,
                         Text(
-                          '작성일: ${DateFormat('yy.MM.dd').format(DateTime.now())}',
+                          '작성일: ${DateFormat('yy.MM.dd').format(createdAt)}',
                           style: AppTextStyles.caption1.copyWith(
                             color: AppColors.sub2Dark,
                           ),

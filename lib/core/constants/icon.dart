112f01add6caedfd35c6edbc503e3a7c2dd5b792
aiden30015 <loolloo080313@gmail.com>
2026-02-19 18:55:37 +0900

    ♻️ :: 아이콘 관리 방식을 상수에서 위젯으로 변경하여 동적 크기 조정 가능하도록 개선

--- lib/core/constants/icon.dart
+++ lib/core/constants/icon.dart
@@ -1,30 +1,71 @@
-/// 앱 아이콘 경로 관리
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+/// 앱 아이콘 관리
 class AppIcons {
   AppIcons._();
 
   static const String _basePath = 'assets/icons';
 
   // Logo
-  static const String logoSmall = '$_basePath/logo_small.svg';
-  static const String logoCircle = '$_basePath/logo_circle.svg';
-  static const String logoSquare = '$_basePath/logo_square.svg';
+  static Widget logoSmall({double size = 24}) =>
+      SvgPicture.asset('$_basePath/logo_small.svg', width: size, height: size);
+
+  static Widget logoCircle({double size = 24}) =>
+      SvgPicture.asset('$_basePath/logo_circle.svg', width: size, height: size);
+
+  static Widget logoSquare({double size = 24}) =>
+      SvgPicture.asset('$_basePath/logo_square.svg', width: size, height: size);
 
   // Navigation
-  static const String home = '$_basePath/home.svg';
-  static const String user = '$_basePath/user.svg';
-  static const String map = '$_basePath/map.svg';
+  static Widget home({double size = 24}) =>
+      SvgPicture.asset('$_basePath/home.svg', width: size, height: size);
+
+  static Widget user({double size = 24}) =>
+      SvgPicture.asset('$_basePath/user.svg', width: size, height: size);
+
+  static Widget map({double size = 24}) =>
+      SvgPicture.asset('$_basePath/map.svg', width: size, height: size);
 
   // Profile
-  static const String profileCircle = '$_basePath/profile_circle.svg';
-  static const String profileSquare = '$_basePath/profile_square.svg';
+  static Widget profileCircle({double size = 24}) => SvgPicture.asset(
+    '$_basePath/profile_circle.svg',
+    width: size,
+    height: size,
+  );
+
+  static Widget profileSquare({double size = 24}) => SvgPicture.asset(
+    '$_basePath/profile_square.svg',
+    width: size,
+    height: size,
+  );
 
   // QR Code
-  static const String qrCodeScan = '$_basePath/qr_code_scan.svg';
-  static const String qrCodeLoad = '$_basePath/qr_code_load.svg';
+  static Widget qrCodeScan({double size = 24}) => SvgPicture.asset(
+    '$_basePath/qr_code_scan.svg',
+    width: size,
+    height: size,
+  );
+
+  static Widget qrCodeLoad({double size = 24}) => SvgPicture.asset(
+    '$_basePath/qr_code_load.svg',
+    width: size,
+    height: size,
+  );
 
   // Status Icons (PNG)
-  static const String successCircle = '$_basePath/success_circle.png';
-  static const String errorCircle = '$_basePath/error_circle.png';
-  static const String bannedCircle = '$_basePath/banned_circle.png';
-  static const String comeBackSuccess = '$_basePath/come_back_success.png';
+  static Widget successCircle({double size = 24}) =>
+      Image.asset('$_basePath/success_circle.png', width: size, height: size);
+
+  static Widget errorCircle({double size = 24}) =>
+      Image.asset('$_basePath/error_circle.png', width: size, height: size);
+
+  static Widget bannedCircle({double size = 24}) =>
+      Image.asset('$_basePath/banned_circle.png', width: size, height: size);
+
+  static Widget comeBackSuccess({double size = 24}) => Image.asset(
+    '$_basePath/come_back_success.png',
+    width: size,
+    height: size,
+  );
 }

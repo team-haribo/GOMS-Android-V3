81b20f5f7effad7ea70b93cf1bc2cb53dc02ada8
aiden30015 <loolloo080313@gmail.com>
2026-02-21 20:45:59 +0900

    ✨ :: 다크 및 라이트 테마에 AppBar 설정 추가 및 GomsAppBar 위젯 구현

--- lib/widgets/common/base_scaffold.dart
+++ lib/widgets/common/base_scaffold.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:project_setting/widgets/common/appbar/goms_app_bar.dart';
 
 class BaseScaffold extends ConsumerWidget {
   final Widget body;
-  final PreferredSizeWidget? appBar;
+  final bool showAppBar;
+  final bool showAppBarLogo;
+  final VoidCallback? onBackPressed;
+  final List<Widget>? appBarActions;
   final Widget? bottomNavigationBar;
   final FloatingActionButton? floatingActionButton;
 
   const BaseScaffold({
     super.key,
     required this.body,
-    this.appBar,
+    this.showAppBar = true,
+    this.showAppBarLogo = false,
+    this.onBackPressed,
+    this.appBarActions,
     this.bottomNavigationBar,
     this.floatingActionButton,
   });
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // showAppBar가 true면 GomsAppBar 사용
+    final effectiveAppBar = showAppBar
+        ? GomsAppBar(
+            showLogo: showAppBarLogo,
+            onBackPressed: onBackPressed,
+            actions: appBarActions,
+          )
+        : null;
+
     return Scaffold(
-      appBar: appBar,
+      appBar: effectiveAppBar,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 52),
         child: body,

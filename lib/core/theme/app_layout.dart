e7a219b3939b6792af8578004e3322b274fbc5f5
suehng0823 <s25010@gsm.hs.kr>
2026-02-25 16:11:05 +0900

    :recycle: :: AppGap 추가

--- lib/core/theme/app_layout.dart
+++ lib/core/theme/app_layout.dart
@@ -5,6 +5,7 @@ class AppSpacing {
   static const double s4 = 4;
   static const double s8 = 8;
   static const double s12 = 12;
+  static const double s20 = 20;
 }
 
 class AppGap {
@@ -16,5 +17,7 @@ class AppGap {
   static const SizedBox v8 = SizedBox(height: AppSpacing.s8);
   static const SizedBox h12 = SizedBox(width: AppSpacing.s12);
   static const SizedBox v12 = SizedBox(height: AppSpacing.s12);
+  static const SizedBox h20 = SizedBox(width: AppSpacing.s20);
+  static const SizedBox v20 = SizedBox(height: AppSpacing.s20);
 
 }

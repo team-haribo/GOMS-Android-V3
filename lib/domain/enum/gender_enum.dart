924acab089ec4900fba1c2780188149a2d8a1a52
aiden30015 <loolloo080313@gmail.com>
2026-02-26 15:40:06 +0900

    ✨:: 성별 및 전공을 나타내는 열거형 추가

---
+++ lib/domain/enum/gender_enum.dart
@@ -0,0 +1,7 @@
+enum GenderEnum {
+  man('남자'),
+  woman('여자');
+
+  const GenderEnum(this.label);
+  final String label;
+}
---
+++ lib/domain/enum/major_enum.dart
@@ -0,0 +1,8 @@
+enum MajorEnum {
+  sw('소프트웨어개발과'),
+  iot('스마트IoT과'),
+  ai('인공지능과');
+
+  const MajorEnum(this.label);
+  final String label;
+}

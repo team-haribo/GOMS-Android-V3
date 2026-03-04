7fe0ce43f3074d6b057ab4180e3f3dea037112c6
suehng0823 <s25010@gsm.hs.kr>
2026-03-04 15:56:38 +0900

    :sparkles: :: 외출 인원 검색 위젯 생성

---
+++ lib/widgets/common/text_fields/search_student.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:project_setting/core/theme/colors/app_colors.dart';
+import 'package:project_setting/core/theme/icons/app_icons.dart';
+import 'package:project_setting/core/theme/layout/app_layout.dart';
+import 'package:project_setting/widgets/common/text_fields/base_text_field.dart';
+
+/// 검색 텍스트 필드
+class SearchStudentField extends StatefulWidget {
+  const SearchStudentField({
+    super.key,
+    this.controller,
+    this.hintText = '학생 검색',
+    this.errorText,
+    this.enabled = true,
+    this.readOnly = false,
+    this.autofocus = false,
+    this.onChanged,
+    this.onSubmitted,
+    this.onBackPressed,
+    this.showBackButton = false,
+    this.showLogo = true,
+  });
+
+  final TextEditingController? controller;
+  final String? hintText;
+  final String? errorText;
+  final bool enabled;
+  final bool readOnly;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+  final ValueChanged<String>? onSubmitted;
+  final VoidCallback? onBackPressed;
+  final bool showBackButton;
+  final bool showLogo;
+
+  @override
+  State<SearchStudentField> createState() => _SearchStudentFieldState();
+}
+
+class _SearchStudentFieldState extends State<SearchStudentField> {
+  late TextEditingController _controller;
+  bool _showClearButton = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+    _controller.addListener(_updateClearButton);
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_updateClearButton);
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void _updateClearButton() {
+    setState(() {
+      _showClearButton = _controller.text.isNotEmpty;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+
+    return BaseTextField(
+      controller: _controller,
+      hintText: widget.hintText,
+      errorText: widget.errorText,
+      keyboardType: TextInputType.text,
+      textInputAction: TextInputAction.search,
+      enabled: widget.enabled,
+      readOnly: widget.readOnly,
+      autofocus: widget.autofocus,
+      onChanged: widget.onChanged,
+      onSubmitted: widget.onSubmitted,
+      suffixIcon: Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: Icon(Icons.search, size: 24, color: isDark ? AppColors.sub2Dark : AppColors.sub2),
+      ),
+    );
+  }
+}

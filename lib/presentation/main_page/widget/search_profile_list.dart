42bb810d7881f284dd2f44c026dd3712a27be0b9
suehng0823 <s25010@gsm.hs.kr>
2026-03-04 21:15:14 +0900

    :bug: :: ci 테스트 오류 수정 (속성 뒤에 , 추가)

--- lib/presentation/main_page/widget/search_profile_list.dart
+++ lib/presentation/main_page/widget/search_profile_list.dart
@@ -42,7 +42,7 @@ class _SearchProfileListState extends State<SearchProfileList> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text('${widget.name}',
+                  Text(widget.name,
                       style: AppTextStyles.text1.copyWith(
                           color:
                               isLight ? AppColors.sub1 : AppColors.sub1Dark,),),

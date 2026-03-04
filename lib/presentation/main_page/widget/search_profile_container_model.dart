42183393bf2a0c024e8003dfc84f3348faec017a
suehng0823 <s25010@gsm.hs.kr>
2026-03-04 15:56:10 +0900

    :sparkles: :: 외출 인원 검색 위젯 생성

---
+++ lib/presentation/main_page/widget/search_profile_container_model.dart
@@ -0,0 +1,11 @@
+class SearchProfileContainerModel {
+  final String name;
+  final int grade;
+  final String major;
+
+  SearchProfileContainerModel({
+    required this.name,
+    required this.grade,
+    required this.major,
+});
+}

cf83170006a8f099e05d84bcd123820d8c6ba3ab
suehng0823 <s25010@gsm.hs.kr>
2026-03-06 19:10:49 +0900

    :sparkles: :: 스크롤 바 추가

---
+++ lib/presentation/map_page/widget/drag_handle_header.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:project_setting/core/theme/colors/app_colors.dart';
+
+class DragHandleHeader extends SliverPersistentHeaderDelegate {
+
+  @override
+  double get minExtent => 40;
+
+  @override
+  double get maxExtent => 40;
+
+  @override
+  Widget build(
+      BuildContext context,
+      double shrinkOffset,
+      bool overlapsContent,
+      ) {
+
+    final isLight = Theme.of(context).brightness == Brightness.light;
+    return Container(
+      color: isLight ?  AppColors.bgSurface : AppColors.bgSurfaceDark,
+      child: Center(
+        child: Container(
+          width: 32,
+          height: 4,
+          decoration: BoxDecoration(
+            color: AppColors.sub2,
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  bool shouldRebuild(covariant DragHandleHeader oldDelegate) {
+    return false;
+  }
+}

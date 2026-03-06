ac934624da690b973730e1ffe32d11c360be3539
suehng0823 <s25010@gsm.hs.kr>
2026-03-06 19:08:08 +0900

    :sparkles: :: 기본 페이지에 사용할 모델 제작

---
+++ lib/presentation/map_page/widget/map_page_models.dart
@@ -0,0 +1,23 @@
+final List<Map<String, dynamic>> popularPlaces = [
+  {
+    'placeName': '메가MGC커피 광주송정시장점',
+    'category': '카페',
+    'address': '광주 광산구 내상로 23 가동 1층',
+    'review': 10,
+    'recommended': 20,
+  },
+  {
+    'placeName': '메가MGC커피 광주송정시장점',
+    'category': '카페',
+    'address': '광주 광산구 내상로 23 가동 1층',
+    'review': 10,
+    'recommended': 20,
+  },
+  {
+    'placeName': '메가MGC커피 광주송정시장점',
+    'category': '카페',
+    'address': '광주 광산구 내상로 23 가동 1층',
+    'review': 10,
+    'recommended': 20,
+  },
+];
---
+++ lib/presentation/map_page/widget/map_page_review_models.dart
@@ -0,0 +1,24 @@
+final List<Map<String, dynamic>> reviewModels = [
+  {
+    'placeName': '메가MGC커피 광주송정시장점',
+    'category' : '카페',
+    'address' : '광주 광산구 내상로 23 가동 1층',
+    'reviewDetailContent' : '굳굳굳 마싯어요',
+    'createdAt' : DateTime.now(),
+  },
+  {
+    'placeName': '메가MGC커피 광주송정시장점',
+    'category' : '카페',
+    'address' : '광주 광산구 내상로 23 가동 1층',
+    'reviewDetailContent' : '굳굳굳 마싯어요',
+    'createdAt' : DateTime.now(),
+  },
+  {
+    'placeName': '메가MGC커피 광주송정시장점',
+    'category' : '카페',
+    'address' : '광주 광산구 내상로 23 가동 1층',
+    'reviewDetailContent' : '굳굳굳 마싯어요',
+    'createdAt' : DateTime.now(),
+  },
+
+];

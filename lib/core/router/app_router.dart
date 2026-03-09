29af3e48bf919868850f5d991ceeaf7328789b25
aiden30015 <loolloo080313@gmail.com>
2026-03-10 00:15:27 +0900

    ♻️ :: 초기 위치를 리뷰 작성 화면에서 스플래시 화면으로 변경

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -23,7 +23,7 @@ final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
 
 final GoRouter router = GoRouter(
   navigatorKey: rootNavigatorKey,
-  initialLocation: RoutePath.writeReview,
+  initialLocation: RoutePath.splash,
   routes: [
     GoRoute(
       path: RoutePath.splash,

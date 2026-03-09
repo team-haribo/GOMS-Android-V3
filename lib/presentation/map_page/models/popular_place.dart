2f746c1727a3db140e6bb4e143647d42d51dac32
aiden30015 <loolloo080313@gmail.com>
2026-03-10 00:05:27 +0900

    ✨ :: PopularPlace 모델 추가 및 MapPageProvider 구현

---
+++ lib/presentation/map_page/models/popular_place.dart
@@ -0,0 +1,15 @@
+class PopularPlace {
+  final String name;
+  final String category;
+  final String address;
+  final int review;
+  final int recommended;
+
+  const PopularPlace({
+    required this.name,
+    required this.category,
+    required this.address,
+    required this.review,
+    required this.recommended,
+  });
+}

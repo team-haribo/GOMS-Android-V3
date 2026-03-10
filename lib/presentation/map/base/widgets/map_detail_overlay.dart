a4875a5fc9174af9ad6e176fdfaeda10a7c05e75
aiden30015 <loolloo080313@gmail.com>
2026-03-10 16:43:16 +0900

    💄 :: 임시 mapDetail 스크린 추가

---
+++ lib/presentation/map/base/widgets/map_detail_overlay.dart
@@ -0,0 +1,19 @@
+﻿import 'package:flutter/material.dart';
+import 'package:project_setting/presentation/map/main/models/map_page_state.dart';
+import 'package:project_setting/presentation/map/main/models/popular_place.dart';
+
+class MapDetailOverlay extends StatelessWidget {
+  final PopularPlace place;
+  final MapPageState state;
+
+  const MapDetailOverlay({
+    super.key,
+    required this.place,
+    required this.state,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}

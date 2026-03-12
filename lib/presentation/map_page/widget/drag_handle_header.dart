7210d1d59bee83f7bbbff8dcf5a871c627ed08ae
suehng0823 <s25010@gsm.hs.kr>
2026-03-13 08:41:14 +0900

    :sparkles: :: 리뷰 보기, 후기 삭제, 후기 신고 스크린 퍼블리싱

--- lib/presentation/map_page/widget/drag_handle_header.dart
+++ lib/presentation/map_page/widget/drag_handle_header.dart
@@ -4,10 +4,10 @@ import 'package:project_setting/core/theme/colors/app_colors.dart';
 class DragHandleHeader extends SliverPersistentHeaderDelegate {
 
   @override
-  double get minExtent => 40;
+  double get minExtent => 36;
 
   @override
-  double get maxExtent => 40;
+  double get maxExtent => 36;
 
   @override
   Widget build(

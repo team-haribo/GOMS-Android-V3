cbc2b60282eda1dbf620554130f2fd6fe503c07d
suehng0823 <s25010@gsm.hs.kr>
2026-03-13 08:40:05 +0900

    :sparkles: :: 도착, 출발 버튼 생성

---
+++ lib/presentation/map_page/widget/arrival_departure_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:project_setting/core/theme/typography/app_text_styles.dart';
+
+class ArrivalDepartureButton extends StatelessWidget {
+  final String buttonText;
+  final Color textColor;
+  final Color backgroundColor;
+
+  const ArrivalDepartureButton({
+    super.key,
+    required this.buttonText,
+    required this.textColor,
+    required this.backgroundColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 89,
+      height: 33,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          elevation: 0,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        ),
+        child: Text(
+          buttonText,
+          style: AppTextStyles.caption1.copyWith(color: textColor),
+        ),
+      ),
+    );
+  }
+}
